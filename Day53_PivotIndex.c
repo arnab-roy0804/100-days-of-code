ea347bfd2dcd4678bfb90170745f42f216c8d293
Arnab Roy <arnab8238@gmail.com>
2025-11-28 00:42:15 +0530

    Add files via upload

---
+++ Day53_PivotIndex.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int totalSum = 0;
+    for (int i = 0; i < n; i++) {
+        totalSum += arr[i];
+    }
+
+    int leftSum = 0;
+    int pivotIndex = -1;
+
+    for (int i = 0; i < n; i++) {
+        int rightSum = totalSum - leftSum - arr[i];
+        if (leftSum == rightSum) {
+            pivotIndex = i;
+            break;  
+        }
+        leftSum += arr[i];
+    }
+
+    printf("Pivot Index: %d\n", pivotIndex);
+
+    return 0;
+}
---
+++ Day56_NextGreaterElement.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int nextGreater = -1;  
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                nextGreater = arr[j];
+                break;  
+            }
+        }
+        
+        printf("%d", nextGreater);
+        if (i != n - 1) {
+            printf(",");  
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
